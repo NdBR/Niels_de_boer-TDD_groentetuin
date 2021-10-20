4efe206aa198d4bfb62326ab2ac64fd8f1e97e24
Niels de Boer <n.d.bo3r@gmail.com>
2021-10-20 16:29:45 +0200

    Added test from template assigment and wordt first function to pass first test getYieldForPlant

---
+++ Testing/farm.js
@@ -0,0 +1,11 @@
+const getYieldForPlant = plant => plant.yield;
+
+const getYieldForCrop = (input) => 0;
+
+const getTotalYield = (crops) => 1;
+
+module.exports = {
+    getYieldForPlant,
+    getYieldForCrop,
+    getTotalYield
+}
