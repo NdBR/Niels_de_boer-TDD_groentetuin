05fc5269482d1d9938c70fd01a466c05478a2cf8
Niels de Boer <n.d.bo3r@gmail.com>
2021-10-20 19:51:15 +0200

    third fucntion getTotalYield added and tested

--- Testing/farm.js
+++ Testing/farm.js
@@ -2,7 +2,14 @@ const getYieldForPlant = plant => plant.yield;
 
 const getYieldForCrop = (input) => input.crop.yield * input.numCrops;
 
-const getTotalYield = (crops) => 1;
+const getTotalYield = (crops) => {
+    const arrayCrops = crops.crops
+    const totalYield = arrayCrops
+        .map(obj => getYieldForCrop(obj))
+        .reduce((currentTotal, item) => item + currentTotal)
+    return totalYield
+}
+
 
 module.exports = {
     getYieldForPlant,
